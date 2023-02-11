6a72f1b567ac64ea6df429cbbb5daadc4fa920cf
Tung <tungak3042000@gmail.com>
2023-02-12 00:27:21 +0700

    tung

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Linh à!',
+    introDesc: `Ấn tiếp sẽ có điều tuyệt vời xảy ra hi`,
+    btnIntro: 'hihi',
+    title: 'thật sự là Anh không muốn muốn quan hệ này tệ đi đâu ạ 😙',
+    desc: 'cho anh xin lỗi vì tất cả Linh nhé !! ',
+    btnYes: 'tha thứ cho anh nhé  <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Tha thứ cho anh nhé Linh <33333333',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/xuantung332000'
+}
